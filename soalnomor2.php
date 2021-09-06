b367a52e6bf7bf411f5397332ff6d525b486e65f
rahayurisky <73808317+rahayurisky@users.noreply.github.com>
2021-09-06 20:27:52 +0700

    Add files via upload

---
+++ soalnomor2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$bio = ['1namaku', '3risky', '5jurusan', '2rahayu', '4yuliani', '8jaringan', '7komputer', '6teknik', '8pemrograman', '9menyukai'];
+asort($bio);
+ print_r($bio);
+ 
+?>
+</body>
+</html>
