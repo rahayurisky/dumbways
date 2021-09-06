a70367bf5839c276ac317e03f0dde5426369c5d9
rahayurisky <73808317+rahayurisky@users.noreply.github.com>
2021-09-06 21:32:09 +0700

    nomor 1
    masih mempelajari

---
+++ soalnomor1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+function jawabansoal_nomor1(){
+	echo "Jawabanya adalah pukul 15.20 WIB nana dan angela berada pada titik yang sama dimana angela mendahului nana";
+}
+jawabansoal_nomor1();
+ 
+?>
+</body>
+</html>
