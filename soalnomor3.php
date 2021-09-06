5c3d1ecf1a302c57af3d16c88b44a1ddce68e3d6
rahayurisky <73808317+rahayurisky@users.noreply.github.com>
2021-09-06 18:31:23 +0700

    Add files via upload

---
+++ soalnomor3.php
@@ -0,0 +1,58 @@
+<?php
+    $star=1;
+	for( $a=$star;$a>0;$a--){
+	for($i=1; $i<=$a; $i++){
+		echo "&nbsp"."&nbsp"."&nbsp"."&nbsp"."&nbsp"."&nbsp".
+        "&nbsp"."&nbsp"."&nbsp"."&nbsp";
+	}
+	for($a1=$star;$a1>=$a;$a1--){
+		echo"D";
+	}
+	echo"<br>";
+	}
+    
+    $star=1;
+	for( $a=$star;$a>0;$a--){
+	for($i=1; $i<=$a; $i++){
+		echo "&nbsp"."&nbsp"."&nbsp"."&nbsp"."&nbsp"."&nbsp".
+        "&nbsp"."&nbsp";
+	}
+	for($a1=$star;$a1>=$a;$a1--){
+		echo"U M";
+	}
+	echo"<br>";
+	}
+    
+     $star=1;
+	for( $a=$star;$a>0;$a--){
+	for($i=1; $i<=$a; $i++){
+		echo "&nbsp"."&nbsp"."&nbsp"."&nbsp"."&nbsp"."&nbsp";
+	}
+	for($a1=$star;$a1>=$a;$a1--){
+		echo"B W A";
+	}
+	echo"<br>";
+	}
+    
+     $star=1;
+	for( $a=$star;$a>0;$a--){
+	for($i=1; $i<=$a; $i++){
+		echo "&nbsp"."&nbsp"."&nbsp"."&nbsp"."&nbsp";
+	}
+	for($a1=$star;$a1>=$a;$a1--){
+		echo"Y  S  I   D";
+	}
+	echo"<br>";
+	}
+    
+     $star=1;
+	for( $a=$star;$a>0;$a--){
+	for($i=1; $i<=$a; $i++){
+		echo "&nbsp"."&nbsp"."&nbsp";
+	}
+	for($a1=$star;$a1>=$a;$a1--){
+		echo"U J I A N";
+	}
+	echo"<br>";
+	}
+?>
